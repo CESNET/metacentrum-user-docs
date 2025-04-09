9cc27a4e6fa7b69dc53f68370669ffcfa21bb0c1
Ivana Krenkova <42272325+ivka99@users.noreply.github.com>
2025-04-09 15:27:35 +0200

    Update footer.tsx
    footer image centering

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Banner from '@/public/img/logos/einfra_meta-zapati.svg';
 export function Footer() {
    return (
      <footer className="mt-auto border-t py-12 text-fd-secondary-foreground ">
-     <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between --color-fd-background relative z-20">
+     <div className="container flex flex-col items-center gap-4 sm:flex-row sm:items-end sm:justify-between --color-fd-background relative z-20">
         <Image src={Banner} alt="publicity banner"/>
      </div>
      </footer>
